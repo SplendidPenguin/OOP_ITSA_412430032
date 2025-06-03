a961c8d132f77b8efc47fff2f0f0369dbe651e6d
SplendidPenguin <6257820@gmail.com>
2025-06-04 03:17:06 +0800

    Add files via upload

---
+++ Q23/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n;
+    cin >> n;
+    if (3 <= n && n <= 5) {
+        cout << "Spring" << endl;
+    } else if (6 <= n && n <= 8) {
+        cout << "Summer" << endl;
+    } else if (9 <= n && n <= 11) {
+        cout << "Autumn" << endl;
+    } else {
+        cout << "Winter" << endl;
+    }
+	return 0;
+}
