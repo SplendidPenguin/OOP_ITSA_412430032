be1eccf965918869c826afe4fe6d4b796122ac59
SplendidPenguin <6257820@gmail.com>
2025-06-04 03:05:41 +0800

    Add files via upload

---
+++ Q16/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n;
+    cin >> n;
+    if (n > 0) cout << "¥¿¼Æ" << endl;
+    else if (n == 0) cout << 0 << endl;
+	else cout << "­t¼Æ" << endl;
+	return 0;
+}
