af9abfab57d05624440228aa9f629e9090c20f2b
SplendidPenguin <6257820@gmail.com>
2025-06-04 02:06:05 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main(void)
 {
-	int h, l;
+	int T, B, H;
     double area;
-    cin >> l >> h;
-    area = (double)(h*l)/2;
-    cout.setf(ios::fixed); //不要指數
-    cout.setf(ios::showpoint);  //要小數點
-    cout.precision(1); //小數點第1位
-    cout << "Triangle area:" << area << endl;
+    cin  >> T >> B >> H;
+    area = (double)(T+B)*H/2;
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(1);
+    cout << "Trapezoid area:" << area <<endl;
+
+
 	return 0;
 }
