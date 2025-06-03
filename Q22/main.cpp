04ab0706c8cf06147f0aa36db96050cca374c82d
SplendidPenguin <6257820@gmail.com>
2025-06-04 03:15:30 +0800

    Add files via upload

---
+++ Q22/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n;
+    cin >> n;
+    switch(n) {
+        case 1:
+            cout << "Person" << endl;
+            break;
+	    case 2:
+            cout << "Fairy" << endl;
+            break;
+	    case 3:
+            cout << "Dwarf" << endl;
+            break;
+        default:
+            break;
+    }//switch°O±o¼gbreak
+	return 0;
+}
